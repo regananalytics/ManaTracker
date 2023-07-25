2e2b1ee11f93153ed1d9d2142822e9493d4dde27
Dan Regan <dan@regan-analytics.com>
2023-07-25 13:08:34 -0600

    now we're talking

--- src/csharp/ManaTracker/Program.cs
+++ src/csharp/ManaTracker/Program.cs
@@ -1,8 +1,2 @@
-﻿namespace ManaTracker;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
