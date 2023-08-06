4f1004396b65b5f2f7989a72f96f02a38e1826b1
Dan Regan <dan@regan-analytics.com>
2023-08-06 13:53:13 -0600

    Check in memory work

--- src/csharp/ManaTracker/Program.cs
+++ src/csharp/ManaTracker/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MemCore;
+
 Console.WriteLine("Hello, World!");
+
+string gameName = "re1";
+var memoryCore = new MemoryCore(gameName);
+
+var state = memoryCore.GetState();
+Console.WriteLine(state.ToString());
