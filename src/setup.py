e60dbe38adcf9860580398c0f0807129b77df643
Dan Regan <dan@regan-analytics.com>
2024-09-02 13:28:05 -0600

    reconfigure setup

--- src/setup.py
+++ src/setup.py
@@ -1,25 +1,61 @@
-from setuptools import setup, find_packages
+from cx_Freeze import setup, Executable
+from pathlib import Path
+# from setuptools import Command
+# import zipfile
+
+from ManaTracker import __about__
+
+name = __about__.__name__
+exe_name = name + '.exe'
+
+# Path mapping
+_src_dir = Path(__file__).parent
+_pkg_dir = _src_dir / name
+_root_dir = _src_dir.parent
+_build_dir = _root_dir / 'build'
+_cfg_dir = _root_dir / 'cfg'
+
+main_script = _pkg_dir / '__main__.py'
+build_dir = _build_dir # ManaTracker/build
+
+excludes = []
+
+include_files = [
+    (_cfg_dir, 'cfg'), 
+]
+
+packages = [
+    __about__.__name__,
+]
+
+executables = [
+    Executable(
+        main_script,
+        base=None,
+        target_name=exe_name
+    )
+]
+
+requirements = []
+with open(_src_dir / 'requirements.txt', 'r') as f:
+    requirements += [line for line in f]
 
 setup(
-    name='ManaTracker',
-    version='0.1.0',
-    author='Dan Regan',
-    author_email='dan@regan-analytics.com',
-    description='Randomizer Tracker App built on C# and Python',
-    url='https://gitlab.com/regananalytics/ManaTracker',
-    license='MIT',
-    packages=find_packages(),
-    include_package_data=True,
-    install_requires=[
-        'dash',
-        'pillow',
-        "pyyaml",
-        "pyzmq",
-        "watchdog"
-    ],
-    entry_points={
-        'console_scripts': [
-            'ManaTracker = ManaTracker.app:main'
-        ]
-    }
+    name=name,
+    version=__about__.__version__,
+    description=__about__.__description__,
+    author=__about__.__author__,
+    author_email=__about__.__author_email__,
+    url=__about__.__url__,
+    license=__about__.__license__,
+    options={
+        'build_exe': {
+            'packages': packages,
+            'excludes': excludes,
+            'include_files': include_files,
+            'build_exe': build_dir
+        },
+    },
+    executables=executables,
+    install_requires=requirements,
 )
