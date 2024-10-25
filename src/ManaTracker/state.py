8ba78c36bdf17288ac8ddf40354ed0aace6e155e
Dan Regan <dan@regan-analytics.com>
2024-10-25 12:41:45 -0600

    include item box in item state

--- src/ManaTracker/state.py
+++ src/ManaTracker/state.py
@@ -1,6 +1,7 @@
 from collections import deque
 import csv
 from pathlib import Path
+import re
 
 from ManaTracker.config import Config
 
@@ -63,45 +64,66 @@ class RE1State(State):
                 for item in row
             ]
         )
-        self.item_states = {k:ItemState(k) for k in self.item_enums}
+        self.inventory = ItemContainer()
+        self.item_box = ItemContainer()
+
 
     def get_item_state(self, enum) -> bool:
-        return 1 if not self._got_update or self.item_states[enum].state else 0
+        if not self._got_update:
+            return True
+        return (
+            self.inventory.items[enum] is not None 
+            or self.item_box.items[enum] is not None
+        )
     
+
+    def get_inventory_quantity(self, enum) -> int:
+        return self.inventory.items[enum]
+    
+
+
     async def callback(self, message: dict):
-        super().callback(message)
-        # Get inventory
-        frame_counter = [v for k,v in message.items() if "FrameCounter" in k]
-        frame = frame_counter[0] if frame_counter != [] else None
-        if frame == 0:
-            self.reset()
-        inventory = {
-            k:v for k,v in message.items()
-            if 'Inventory' in k and 'Item' in k
-        }
-        for key, enum in inventory.items():
-            if enum == 0:
+        self._got_update = True
+
+        inventory_map = {}
+        item_box_map = {}
+        for k,v in message.items():
+            inventory_match = re.search(r'Inventory\[(\d+)\]\.Item', k)
+            if inventory_match and v > 0:
+                slot = int(inventory_match.group(1))
+                inventory_map[slot] = [v, 0]
                 continue
-            if enum in self.item_enums:
-                key_num = int(key.split('[')[1].split(']')[0])
-                if key_num < 8:
-                    quantity = message[f'Inventory[{key_num}].Quantity']
-                    self.item_states[enum].quantity = quantity
+            inventory_match = re.search(r'Inventory\[(\d+)\]\.Quantity', k)
+            if inventory_match:
+                slot = int(inventory_match.group(1))
+                if slot in inventory_map:
+                    inventory_map[slot][1] = v
+                continue
+            item_box_match = re.search(r'ItemBox\[(\d+)\]\.Item', k)
+            if item_box_match and v > 0:
+                slot = int(item_box_match.group(1))
+                item_box_map[slot] = [v, 0]
+                continue
+            item_box_match = re.search(r'ItemBox\[(\d+)\]\.Quantity', k)
+            if item_box_match:
+                slot = int(item_box_match.group(1))
+                if slot in item_box_map:
+                    item_box_map[slot][1] = v
+                continue
+        
+        self.inventory.update(list(inventory_map.values()))
+        self.item_box.update(list(item_box_map.values()))
 
-    def reset(self):
-        for state in self.item_states.values():
-            state.reset()
 
+class ItemContainer:
+    def __init__(self, nitems=256):
+        self.nitems = nitems
+        self.items = [0]*nitems
 
-class ItemState:
-
-    def __init__(self, enum: int):
-        self.enum = enum
-        self.quantity: int = 0
-    
-    @property
-    def state(self) -> bool:
-        return self.quantity > 0
-
-    def reset(self):
-        self.quantity = 0
+    def update(self, map: list[list[int]]):
+        self.items = [None]*self.nitems
+        for item_quantity in map:
+            if self.items[item_quantity[0]] is None:
+                self.items[item_quantity[0]] = item_quantity[1]
+            else:
+                self.items[item_quantity[0]] += item_quantity[1]
